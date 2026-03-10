59a5607612b6394432db67de6f2fbc61b6d42e14
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-11 00:55:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Ye line add karne se Vercel ko pata chalega ki output kahan hai
+  output: 'standalone',
+};
 
 export default nextConfig;
